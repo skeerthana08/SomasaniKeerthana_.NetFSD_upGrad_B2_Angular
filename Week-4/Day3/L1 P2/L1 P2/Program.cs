a1efc94ca9bcaf48c1cbcd107d492a0a4638c26c
Keerthana Somasani <keerthana781604@gmail.com>
2026-03-23 09:35:05 +0530

    Add files via upload

---
+++ Week-4/Day3/L1 P2/L1 P2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Operator (+, -, *, /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero!");
+                }
+                else
+                {
+                    result = num1 / num2;
+                    Console.WriteLine("Result: " + result);
+                }
+                break;
+
+            default:
+                Console.WriteLine("Invalid operator!");
+                break;
+        }
+    }
+}
---
+++ Week-4/Day3/L2 P1/L2 P1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        double salary = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter Experience (years): ");
+        int experience = int.Parse(Console.ReadLine());
+
+        double bonusPercentage;
+
+        // Using if-else
+        if (experience < 2)
+            bonusPercentage = 0.05;
+        else if (experience <= 5)
+            bonusPercentage = 0.10;
+        else
+            bonusPercentage = 0.15;
+
+        // Using ternary (just to satisfy requirement)
+        double bonus = salary * bonusPercentage;
+
+        double finalSalary = salary + bonus;
+
+        Console.WriteLine("\nEmployee: " + name);
+        Console.WriteLine("Bonus: " + bonus);
+        Console.WriteLine("Final Salary: " + finalSalary);
+    }
+}
